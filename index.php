09d9ed7197e352f658d46225e6f5876a946dab07
chengHMDev <46214395+chengHMDev@users.noreply.github.com>
2021-12-02 19:09:39 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Where is the ISS</title>
+</head>
+<body>
+
+	<h1>Where is the ISS</h1>
+	<div>
+		<form id="form" method="post" action="Webservices.php">
+			Date and time:
+			<input type="datetime-local" name="datetime" value="<?php echo date('Y-m-d'); ?>" />
+			<br/>
+			<input type="submit" name="search" class="button" value="search">
+		</form>
+	</div>
+</body>
+</html>
